ba9c7b92ca113055ebcba05b69545645dd77b8b4
k-cpa <kevcampana@gmail.com>
2025-05-20 18:02:15 +0200

    affichage random mots pour haiku + gestion notificaiton à la création du haiku

--- src/Controller/WriteHaikuController.php
+++ src/Controller/WriteHaikuController.php
@@ -4,8 +4,10 @@ namespace App\Controller;
 
 use App\Entity\Haikus;
 use App\Entity\User;
+use App\Entity\UserWords;
 use App\Form\WriteHaikuType;
 use App\Repository\UserWordsRepository;
+use App\Service\NotificationService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -15,18 +17,16 @@ use Symfony\Component\Routing\Attribute\Route;
 final class WriteHaikuController extends AbstractController
 {
     #[Route('/create/haiku', name: 'app_create_haiku')]
-    public function write(Request $request, EntityManagerInterface $entityManager, UserWordsRepository $userWords): Response
+    public function write(Request $request, EntityManagerInterface $entityManager, UserWordsRepository $userWords, NotificationService $notificationService): Response
     {
         $packs = $userWords->findPendingPacksForUser();
 
-        // On reprend le premier pack trouvé sinon on met null
-        $pack = $packs[0] ?? null;
-
-        $senderUser = $entityManager->getRepository(User::class)->find($pack['sender_id']);
+        // On reprend un pack random sinon on met null
+        $pack = $packs[array_rand($packs)] ?? null;
 
         if ($pack) {
             $userWordsList = $userWords->findBy([
-                'sender' => $senderUser,
+                'sender' => $pack['sender_id'],
                 'created_at' => $pack['created_at'],
                 'status' => 'pending',
                 'Receiver' => null,
@@ -44,14 +44,15 @@ final class WriteHaikuController extends AbstractController
         $formHaiku->handleRequest($request);
 
         if ($formHaiku->isSubmitted() && $formHaiku->isValid()) {
+            
             $haikuContent = $formHaiku->getData();
             
             $haiku->setContent($haikuContent);
             $haiku->setCreator($currentUser);
-            // $haiku->setUserWords() voir après comment intégrer ça 
             $entityManager->persist($haiku);
-            $words = [];
 
+            // On initialise un tableau vide pour les mots
+            $words = [];
 
             foreach ($userWordsList as $userWord) {
 
@@ -62,6 +63,18 @@ final class WriteHaikuController extends AbstractController
                 $entityManager->persist($userWord);
             }
 
+            // Un premier flush obligatoire pour récupérer l'id du haiku -> optimisation possible ?
+            $entityManager->flush();
+
+
+            // Création de la notification
+            $notificationService->createNotification(
+                $currentUser,
+                $userWordsList[0]->getSender(),
+                'haiku',
+                $haiku->getId(),
+            );
+            
             $entityManager->flush();
 
             return $this->redirectToRoute('app_feed');
