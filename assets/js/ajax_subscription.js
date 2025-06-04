0e860e4c0199b92a3f02529bdc1ed72485963e9c
k-cpa <kevcampana@gmail.com>
2025-06-04 10:12:45 +0200

    coucou Ludo

--- assets/js/ajax_subscription.js
+++ assets/js/ajax_subscription.js
@@ -6,41 +6,39 @@ document.addEventListener('DOMContentLoaded', function () {
     subscribeButtons.forEach(button => {
         button.addEventListener('click', async (event) => {
             event.preventDefault();
-            if (event.target && event.target.classList.contains('subscribe-button')) {
+            
+            const userId = event.target.dataset.userId;
 
-                const userId = event.target.dataset.userId;
-
-                fetch(`/profil/${userId}/subscription`, {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                        'X-Requested-With': 'XMLHttpRequest',
-                        'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
-                    },
-                    body: JSON.stringify({ id: userId })
-                })
-                .then(response => response.json())
-                .then(data => {
-                    if (!data.success) {
-                        console.log('Erreur avec l\'appel AJAX');
-                        return;
-                    }
-                    const isSubscribed = data.subscribed; // true = abonné, false = désabonné
-                    if (isSubscribed) {
-                        event.target.textContent = 'Se désabonner';
-                        event.target.classList.add('subscribed');
-                        event.target.setAttribute('data-subscribed', 'true');
-                    } else {
-                        event.target.textContent = 'S’abonner';
-                        event.target.classList.remove('subscribed');
-                        event.target.setAttribute('data-subscribed', 'false');
-                    }
-                    console.log('Data :', data)
-                })
-                .catch(error => {
-                    console.error('Erreur AJAX:', error);
-                });
-            }
+            fetch(`/profil/${userId}/subscription`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'X-Requested-With': 'XMLHttpRequest',
+                    'X-CSRF-Token': document.querySelector('meta[name="csrf-token"]').getAttribute('content'),
+                },
+                body: JSON.stringify({ id: userId })
+            })
+            .then(response => response.json())
+            .then(data => {
+                if (!data.success) {
+                    console.log('Erreur avec l\'appel AJAX');
+                    return;
+                }
+                const isSubscribed = data.subscribed; // true = abonné, false = désabonné
+                if (isSubscribed) {
+                    event.target.textContent = 'Se désabonner';
+                    event.target.classList.add('subscribed');
+                    event.target.setAttribute('data-subscribed', 'true');
+                } else {
+                    event.target.textContent = 'S’abonner';
+                    event.target.classList.remove('subscribed');
+                    event.target.setAttribute('data-subscribed', 'false');
+                }
+                console.log('Data :', data)
+            })
+            .catch(error => {
+                console.error('Erreur AJAX:', error);
+            });
         });
     });
 });
