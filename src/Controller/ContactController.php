06171a33e47c90a07c8fe9e207b15db6738d28d5
k-cpa <kevcampana@gmail.com>
2025-06-12 17:28:11 +0200

    formulaire de contact

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ContactController extends AbstractController
+{
+    #[Route('/landing/contact', name: 'app_contact')]
+    public function contactForm(Request $request, MailerInterface $mailer): Response
+    {
+
+        $contactForm = $this->createForm(ContactType::class);
+        $contactForm->handleRequest($request);
+
+        if($contactForm->isSubmitted() && $contactForm->isValid()) {
+            $data = $contactForm->getData(); // Récupération des champs du formulaire
+
+            $email = (new Email())
+                ->from('07148281344577@mailtrap.io')
+                ->to('kevcampana@gmail.com')
+                ->subject('Nouveau message de contact')
+                ->text(
+                    "Nom: {$data['nom']}\n".
+                    "Email: {$data['email']}\n\n".
+                    "Message:\n{$data['message']}"
+                );
+                $mailer->send($email);
+                
+            return $this->redirectToRoute('app_feed');
+
+                
+        }
+        return $this->render('landing/contact.html.twig', [
+            'contactForm' => $contactForm->createView(),
+        ]);
+    }
+}
